2e4e9492f4ff3925b1550d451f6bfaa035ccc913
Philip Yap <philipyap@Philips-MacBook.local>
2020-08-26 16:46:00 -0700

    [todolist]added clear list, new item, add item, and new item change

--- src/MyList.js
+++ src/MyList.js
@@ -4,8 +4,46 @@ import './App.css'
 import ListItem from './ListItem'
 
 class MyList extends Component {
+
+  constructor(props){
+    super()
+    this.state = {
+      toDoItemArray: props.theList,
+      newItem: " "
+    }
+  }
+  //delete list
+  clearList = () => {
+    console.log("clearing list!")
+    this.setState({
+      toDoItemArray: []
+    })
+  }
+
+  //update new change
+  newItemChange = e => {
+    this.setState({
+      newItem: e.target.value
+    })
+  }
+
+  //add new items
+  addItem = e => {
+    console.log("add list")
+    console.log("before delete", this.state.newItem)
+    e.preventDefault()
+    const newArray = this.state.toDoItemArray;
+    newArray.push(this.state.newItem);
+    this.setState({
+      toDoItemArray: newArray,
+      newItem: " "
+    })
+    console.log("after", this.state.newItem)
+  }
+
+
   render() {
-    let todoItems = this.props.theList.map((item, index) => (
+    let todoItems = this.state.toDoItemArray.map((item, index) => (
       <ListItem doThis={item} key={index} />
     ))
 
@@ -15,6 +53,15 @@ class MyList extends Component {
         <ul>
           {todoItems}
         </ul>
+        <form>
+          <input type="text"
+          placeholder="Type an item here"
+          onChange={(e) => this.newItemChange(e)}
+          value={this.state.newItem}
+          />
+          <button onClick={(e) => this.addItem(e)}>Add it!</button>
+        </form>
+        <button onClick={this.clearList}>Finished the list!</button>
       </div>
     )
   }
